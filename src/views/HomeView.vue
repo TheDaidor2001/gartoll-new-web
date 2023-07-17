855923a11e3f1d3bc2cf51553f490b861195e7b3
TheDaidor2001 <danielcastillo1552001@hotmail.com>
2023-07-17 10:57:31 +0100

    added section and responsive

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,6 @@
 <script setup>
 import cardInfo from '../components/UI/card-info.vue';
+
 </script>
 
 <template>
@@ -15,7 +16,8 @@ import cardInfo from '../components/UI/card-info.vue';
         <h1 class="text-4xl lg:text-7xl text-center font-black  text-secondary uppercase">Gartoll</h1>
         <h2 class="text-center text-gray-900 text-3xl lg:text-5xl font-black uppercase">Seguridad Privada</h2>
         <div class="w-full xl:w-2/3 mx-auto">
-          <p class="mt-5 text-center text-lg lg:text-xl text-gray-700">Nuestro equipo de seguridad privada está preparado
+          <p class="mt-5 text-center mx-auto text-gray-700 px-10 lg:px-0">Nuestro equipo de seguridad privada está
+            preparado
             para protegerlo en todo momento, brindando tranquilidad, confianza y servicio impecable con profesionalismo y
             dedicación.</p>
         </div>
@@ -55,7 +57,20 @@ import cardInfo from '../components/UI/card-info.vue';
         <path stroke-linecap="round" stroke-linejoin="round"
           d="M9.594 3.94c.09-.542.56-.94 1.11-.94h2.593c.55 0 1.02.398 1.11.94l.213 1.281c.063.374.313.686.645.87.074.04.147.083.22.127.324.196.72.257 1.075.124l1.217-.456a1.125 1.125 0 011.37.49l1.296 2.247a1.125 1.125 0 01-.26 1.431l-1.003.827c-.293.24-.438.613-.431.992a6.759 6.759 0 010 .255c-.007.378.138.75.43.99l1.005.828c.424.35.534.954.26 1.43l-1.298 2.247a1.125 1.125 0 01-1.369.491l-1.217-.456c-.355-.133-.75-.072-1.076.124a6.57 6.57 0 01-.22.128c-.331.183-.581.495-.644.869l-.213 1.28c-.09.543-.56.941-1.11.941h-2.594c-.55 0-1.02-.398-1.11-.94l-.213-1.281c-.062-.374-.312-.686-.644-.87a6.52 6.52 0 01-.22-.127c-.325-.196-.72-.257-1.076-.124l-1.217.456a1.125 1.125 0 01-1.369-.49l-1.297-2.247a1.125 1.125 0 01.26-1.431l1.004-.827c.292-.24.437-.613.43-.992a6.932 6.932 0 010-.255c.007-.378-.138-.75-.43-.99l-1.004-.828a1.125 1.125 0 01-.26-1.43l1.297-2.247a1.125 1.125 0 011.37-.491l1.216.456c.356.133.751.072 1.076-.124.072-.044.146-.087.22-.128.332-.183.582-.495.644-.869l.214-1.281z" />
         <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
-    </svg>
+      </svg>
+    </cardInfo>
+  </section>
 
-  </cardInfo>
-</section></template>
+  <section class="py-10 px-10 lg:px-0 mx-auto container flex flex-col-reverse lg:flex-row justify-center items-center gap-10 text-center lg:text-left">
+      <div class="flex flex-col gap-5 w-full lg:w-1/3">
+        <h3 class="text-3xl lg:text-4xl font-normal text-gray-600">Tu empresa de <span class="font-black">Seguridad Privada</span></h3>
+        <p class="text-gray-500 font-thin text-md">Con más de una década de experiencia en el sector nos enorgullecemos de ser una opción de seguridad privada confiable y dedicada.</p>
+        <p class="text-gray-500 font-thin text-md ">Hemos estado comprometidos en salvaguardar lo que más valoras, brindando tranquilidad a nuestros clientes en cada paso del camino. Nuestra trayectoria sólida y exitosa es el reflejo de un equipo altamente capacitado y apasionado por proteger a comunidades, hogares y negocios.</p>
+        <button
+          class="text-center text-xl lg:text-lg font-bold block bg-secondary mt-5 text-white px-5 py-2 rounded-lg hover:bg-amber-300 transition-colors w-full lg:w-1/3 md:w-2/3 shadow-md mx-auto lg:mx-0">
+          Saber más
+        </button>
+      </div>
+      <img class="w-[500px]" src="/seguridad.webp" alt="">
+  </section>
+</template>
